beba1f1a4403e094a3f20444f628d66bea7c2c9f
Kunjesh Patel <74760068+Kunjesh07@users.noreply.github.com>
2021-03-14 16:55:13 +0530

    Update KrabbyBot.py

--- KrabbyBot.py
+++ KrabbyBot.py
@@ -32,7 +32,7 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 import googletrans
 from googletrans import Translator
-from newsapi import NewsApiClient
+#from newsapi import NewsApiClient
 
 app = Flask(__name__)
 
