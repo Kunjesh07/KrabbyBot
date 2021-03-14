30cba2f08bac7c246793d21a6e438dcb7cf2892c
Kunjesh Patel <74760068+Kunjesh07@users.noreply.github.com>
2021-03-14 16:43:26 +0530

    Update KrabbyBot.py

--- KrabbyBot.py
+++ KrabbyBot.py
@@ -18,21 +18,21 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-#LICENSE   = MIT GutHub
+#LICENSE   = MIT GitHub
 #__author__= KUNJESH PATEL
 #__email___= kunjesh137@gmail.com
 #__Year____= 2020
 #Version___= 1.1.0
 
-from flask import Flask, request, jsonify
-import json
-import requests
-from twilio.twiml.messaging_response import MessagingResponse
-from googletrans import Translator
-import wikipedia
 import datetime
 
-
+import requests
+import wikipedia
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import googletrans
+from googletrans import Translator
+from newsapi import NewsApiClient
 
 app = Flask(__name__)
 
@@ -73,21 +73,25 @@ def bot():
         text = f'*Info Coronavirus India 🇮🇳*\n\n*Positive* : {js["cases"]} \n*Recovred* : {js["recovered"]} \n*Died* : {js["deaths"]} \n*Today cases* : {js["todayCases"]} \n*Today died* : {js["todayDeaths"]} \n*critical* : {js["critical"]} \n*casesPerOneMillion* : {js["casesPerOneMillion"]} \n*deathsPerOneMillion* : {js["deathsPerOneMillion"]} \n*totalTests* : {js["totalTests"]} \n*TestsPerMillion*: {js["testsPerOneMillion"]}\n\n\n*Info Coronavirus USA 🇺🇸*\n\n*Positive* : {jsu["cases"]} \n*Recovred* : {jsu["recovered"]} \n*Died* : {jsu["deaths"]} \n*Today cases* : {jsu["todayCases"]} \n*Today died* : {jsu["todayDeaths"]} \n*critical* : {jsu["critical"]} \n*casesPerOneMillion* : {jsu["casesPerOneMillion"]} \n*deathsPerOneMillion* : {jsu["deathsPerOneMillion"]} \n*totalTests* : {jsu["totalTests"]} \n*TestsPerMillion*: {jsu["testsPerOneMillion"]} \n\n\n*Info Coronavirus Russia 🇷🇺*\n\n*Positive* : {jsr["cases"]} \n*Recovred* : {jsr["recovered"]} \n*Died* : {jsr["deaths"]} \n*Today cases* : {jsr["todayCases"]} \n*Today died* : {jsr["todayDeaths"]} \n*critical* : {jsr["critical"]} \n*casesPerOneMillion* : {jsr["casesPerOneMillion"]} \n*deathsPerOneMillion* : {jsr["deathsPerOneMillion"]} \n*totalTests* : {jsr["totalTests"]} \n*TestsPerMillion*: {jsr["testsPerOneMillion"]} \n\n\n*Global* \n\n*Positive* : {jss["cases"]} \n*Recovered* : {jss["recovered"]} \n*Died* : {jss["deaths"]}'
         msg.body(text)
         responded = True
-    
+       
+
+    #if "currency" in incoming_msg:
+        
+
     if 'Menu' in incoming_msg or 'menu' in incoming_msg:
         text = f'⌨️ *List Of Command :*  \n\n🔥 *info-covid* (Information of COVID-19) \n\n🔥 *Schedule* _Display Schedule_\n\n🔥 */YT* _<url>_ : Youtube Downloader\n\n🔥 */Quote* : Generate Quote\n\n🔥 */wiki* : Information form wikipedia\n\n🔥 */FL* _<url>_ : Download Big Size Fb Videos\n\n🔥 */GL* _<query>_ : Google Search\n\n🔥 */weather* : weather Information \n\n🔥 */TTS* <Text> : Text To Speech\n\n🔥 *help* : How to use the command'
         msg.body(text)
         responded = True
     
-    if '/GL' in incoming_msg:
+    if 'GL' in incoming_msg or 'gl' in incoming_msg:
         from googlesearch import search
-        query = incoming_msg[3:]
+        query = incoming_msg[2:]
         for i in search(query, tld="com", num=10, stop=10, pause=2):
             text = f'\n\n============Results============\n\n *Result* : '+i
             msg.body(text)
             responded = True
 
-    if '/quote' in incoming_msg:
+    if 'quote' in incoming_msg or 'Quote' in incoming_msg:
         # return a quote
         r = requests.get('https://api.quotable.io/random')
         if r.status_code == 200:
@@ -98,7 +102,7 @@ def bot():
         msg.body(quote)
         responded = True
 
-    if '/weather' in incoming_msg or '/Weather' in incoming_msg:
+    if 'weather' in incoming_msg or 'Weather' in incoming_msg:
         api_key = "8ef61edcf1c576d65d836254e11ea420"
         base_url = "https://api.openweathermap.org/data/2.5/weather?"
         city_name= ("Ahmedabad")    
@@ -119,56 +123,181 @@ def bot():
         msg.body(text3)
         responded = True
 
-    if '/location' in incoming_msg:
-        def index():
-            url = 'http://freegeoip.net/json'
-            r = requests.get(url)
-            j = json.loads(r.text)
-            city = j['city']
-            text = f'Your City is :' + city
-            msg.body(text)
-            responded = True
-        # from flask_simple_geoip import SimpleGeoIP
-        #app.config.update(GEOIPIFY_API_KEY='')
-        #simple_geoip = SimpleGeoIP(app)
-        #geoip_data = simple_geoip.get_geoip_data()
-        #jsonify=jsonify(data=geoip_data)
-        #text = f'Current location :' + jsonify
-       
-        
-    if '/wiki' in incoming_msg:
-        query = incoming_msg[10:]
+    if '/TR-en-gu' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='en', dest='gu')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-gu-en' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='gu', dest='en')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-en-mr' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='en', dest='mr')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-mr-en' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='mr', dest='en')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-en-hi' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='en', dest='hi')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-hi-en' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='hi', dest='en')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-en-kn' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='en', dest='kn')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-kn-en' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='kn', dest='en')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-en-bn' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='en', dest='bn')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-bn-en' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='bn', dest='en')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-en-ta' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='en', dest='ta')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-ta-en' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='ta', dest='en')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-en-te' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='en', dest='te')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-te-en' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='te', dest='en')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-en-ml' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='en', dest='ml')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-ml-en' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='ml', dest='en')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-en-pa' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='en', dest='pa')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-pa-en' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='pa', dest='en')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-en-ur' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='en', dest='ur')
+        msg.body(result.text)
+        responded = True
+
+    if '/TR-ur-en' in incoming_msg:
+        par = incoming_msg[9:]
+        translator = Translator()
+        result = translator.translate(par, src='ur', dest='en')
+        msg.body(result.text)
+        responded = True
+
+    if 'wiki' in incoming_msg or 'Wiki' in incoming_msg:
+        query = incoming_msg[15:]
         incoming_msg = incoming_msg.replace("wiki", "")
-       	result = wikipedia.summary(incoming_msg, sentences=3)
-        text = f'\n\n This is according to wikipedia\n\n=============*Result*==============\n\n: '+result
+        result = wikipedia.summary(incoming_msg, sentences=3)
+        text = f'\n\n This is according to wikipedia\n\n=============*Result*==============\n\n '+result
         msg.body(text)
         responded = True
 
     if 'Time' in incoming_msg or 'time' in incoming_msg:
         strTime=datetime.datetime.now().strftime("%H:%M:%S")
-        text = f'\n\n Current time is\n: '+ strTime
+        text = f'\n\n Current time \n '+ strTime
         msg.body(text)
         responded = True
-            
-    if '/FL' in incoming_msg:
+
+    if 'FL' in incoming_msg or 'fl' in incoming_msg:
         import requests as r
         import re
-        par = incoming_msg[3:]
+        par = incoming_msg[2:]
         html = r.get(par)
         video_url = re.search('sd_src:"(.+?)"', html.text).group(1)
         reqq = r.get('http://tinyurl.com/api-create.php?url='+video_url)
         msg.body('*Video works on Convert...*\n\nLINK : '+reqq.text)
         responded = True
-        
-    if '/TTS' in incoming_msg:
-        par = incoming_msg[5:]
-        msg.media('https://api.farzain.com/tts.php?id='+par+'&apikey=JsaChFteVJakyjBa0M5syf64z&')
-        responded = True
 
-    if '/YT' in incoming_msg:
+
+        
+   # if '/TTS' in incoming_msg:
+      #  par = incoming_msg[5:]
+       # msg.media('https://api.farzain.com/tts.php?id='+par+'&apikey=JsaChFteVJakyjBa0M5syf64z&')
+       # responded = True
+
+    if 'YT' in incoming_msg or 'yt' in incoming_msg:
         import pafy
         import requests as r
-        par = incoming_msg[4:]
+        par = incoming_msg[3:]
         audio = pafy.new(par)
         gen = audio.getbestaudio(preftype='m4a')
         genn = audio.getbestvideo(preftype='mp4')
@@ -177,8 +306,8 @@ def bot():
         msg.body('_=========================_\n\n     _Video Converted Successfully_\n\n_=========================_\n\n''*'+gen.title+'*''\n\n*Link Download Music* :' +req.text+'\n\n*Link Download Video* :' +popo.text)
         responded = True
         
-    if 'Schedule' in incoming_msg:
-       msg.media('https://user-images.githubusercontent.com/74760068/102241016-82258d80-3f1e-11eb-8c71-6e0176c2739c.png')
+    if 'Schedule' in incoming_msg or 'schedule' in incoming_msg:
+       msg.media('https://user-images.githubusercontent.com/74760068/111062692-72dbbf00-84d0-11eb-9f46-2b88854c78a0.png')
        responded = True
 
     if 'help' in incoming_msg or 'Help' in incoming_msg:
